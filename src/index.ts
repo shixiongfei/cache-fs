62e402a93cf7877e3fb145e457444a7763e09b53
shixiongfei <xiongfei.shi@icloud.com>
2024-05-23 13:34:56 +0800

    Asynchronous IO

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,10 @@
  */
 
 import fs from "node:fs";
+import fsp from "node:fs/promises";
 import path from "node:path";
 import zlib from "node:zlib";
+import { promisify } from "node:util";
 import { isPromise } from "node:util/types";
 import { encode, decode } from "msgpackr";
 
@@ -47,23 +49,45 @@ export const cacheFs = <T, P extends Array<unknown>>(
   return cachedFn;
 };
 
+const existsFile = async (filename: string) => {
+  try {
+    await fsp.access(filename);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const ensurePathAsync = async (filename: string) => {
+  const filepath = path.dirname(filename);
+
+  if (!(await existsFile(filepath))) {
+    await fsp.mkdir(filepath, { recursive: true });
+  }
+
+  return filename;
+};
+
+const unzip = promisify(zlib.unzip);
+const gzip = promisify(zlib.gzip);
+
 export const cacheFsAsync = <T, P extends Array<unknown>>(
   pathGen: (...args: P) => Promise<string> | string,
   fn: (...args: P) => Promise<T> | T,
 ) => {
   const cachedFn = async (...args: P): Promise<T> => {
     const path = pathGen.apply(this, args);
-    const filename = ensurePath(isPromise(path) ? await path : path);
+    const filename = await ensurePathAsync(isPromise(path) ? await path : path);
 
-    if (fs.existsSync(filename)) {
-      const data = fs.readFileSync(filename);
-      return decode(zlib.unzipSync(data));
+    if (await existsFile(filename)) {
+      const data = await fsp.readFile(filename);
+      return decode(await unzip(data));
     } else {
       const result = fn.apply(this, args);
       const retval = isPromise(result) ? await result : result;
 
       if (retval !== undefined) {
-        fs.writeFileSync(filename, zlib.gzipSync(encode(retval)));
+        await fsp.writeFile(filename, await gzip(encode(retval)));
       }
       return retval;
     }
